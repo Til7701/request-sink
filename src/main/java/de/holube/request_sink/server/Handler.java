3cdca48abfdd8c545adfe0eaf976bb8df30a6f85
Tilman Holube <tilman@holube.de>
2025-07-04 11:49:02 +0200

    Status code support and JavaDoc (#7)

--- src/main/java/de/holube/request_sink/server/Handler.java
+++ src/main/java/de/holube/request_sink/server/Handler.java
@@ -11,22 +11,42 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * This class receives requests and uses the {@link OutputBuilder} to construct the output.
+ * Each request uses only one print statement to reduce the impact of parallel requests.
+ * <p>
+ * After the request is printed, the set status code is returned and the exchange closed.
+ */
 public final class Handler implements HttpHandler {
 
     private static final AtomicInteger requestCounter = new AtomicInteger();
+
+    private final int statusCode;
+
+    /**
+     * Constructs a new handler to use with an {@link com.sun.net.httpserver.HttpServer}.
+     *
+     * @param statusCode the status code to return to requests
+     */
+    public Handler(int statusCode) {
+        this.statusCode = statusCode;
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         OutputBuilder ob = new OutputBuilder();
 
+        // Metadata
+        ob.setId(requestCounter.getAndIncrement());
         ob.addMetadataLine("Time", Instant.now().toString());
         ob.addMetadataLine("Method", exchange.getRequestMethod());
         ob.addMetadataLine("URI", exchange.getRequestURI().toString());
-        ob.setId(requestCounter.getAndIncrement());
 
+        // Headers
         for (Map.Entry<String, List<String>> entry : exchange.getRequestHeaders().entrySet())
             entry.getValue().forEach(v -> ob.addHeaderLine(entry.getKey(), v));
 
+        // Body
         try {
             String body = new String(exchange.getRequestBody().readAllBytes());
             ob.setBody(body);
@@ -36,7 +56,7 @@ public final class Handler implements HttpHandler {
 
         IO.println(ob.build());
 
-        exchange.sendResponseHeaders(200, -1);
+        exchange.sendResponseHeaders(statusCode, -1);
         exchange.close();
     }
 
