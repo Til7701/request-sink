1c9701ab7058c5d646e9ebcda7b48b5863a25905
Tilman Holube <tilman@holube.de>
2025-09-28 01:11:35 +0200

    Tests

--- src/test/java/de/holube/request_sink/validation/http/status_code/HttpStatusCodeCsvReaderTest.java
+++ src/test/java/de/holube/request_sink/validation/http/status_code/HttpStatusCodeCsvReaderTest.java
@@ -16,31 +16,39 @@ class HttpStatusCodeCsvReaderTest {
 
         // first
         HttpStatusCode statusCode = statusCodes.get(100);
-        assertNotNull(statusCode);
         assertEquals(100, statusCode.code());
         assertEquals("Continue", statusCode.description());
         assertEquals(HttpStatusCodeStatus.STANDARD, statusCode.status());
 
         // any standard code
         statusCode = statusCodes.get(200);
-        assertNotNull(statusCode);
         assertEquals(200, statusCode.code());
         assertEquals("OK", statusCode.description());
         assertEquals(HttpStatusCodeStatus.STANDARD, statusCode.status());
 
         // any unassigned code
         statusCode = statusCodes.get(150);
-        assertNotNull(statusCode);
         assertEquals(150, statusCode.code());
         assertEquals("Unassigned", statusCode.description());
         assertEquals(HttpStatusCodeStatus.UNASSIGNED, statusCode.status());
 
         // any unused code
         statusCode = statusCodes.get(306);
-        assertNotNull(statusCode);
         assertEquals(306, statusCode.code());
         assertEquals("(Unused)", statusCode.description());
         assertEquals(HttpStatusCodeStatus.UNUSED, statusCode.status());
+
+        // last standard code
+        statusCode = statusCodes.get(511);
+        assertEquals(511, statusCode.code());
+        assertEquals("Network Authentication Required", statusCode.description());
+        assertEquals(HttpStatusCodeStatus.STANDARD, statusCode.status());
+
+        // last unassigned code
+        statusCode = statusCodes.get(599);
+        assertEquals(599, statusCode.code());
+        assertEquals("Unassigned", statusCode.description());
+        assertEquals(HttpStatusCodeStatus.UNASSIGNED, statusCode.status());
 
         // any out of range code
         statusCode = statusCodes.get(999);
